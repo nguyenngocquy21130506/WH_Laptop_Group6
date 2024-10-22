bfd7cbb267f5dfe3484077e2f1e22d1d96e81488
nnquy <21130506@st.hcmuaf.edu.vn>
2024-10-22 08:58:11 +0700

    init project spring

---
+++ warehouse/src/main/java/com/group6/warehouse/WhLaptopApplication.java
@@ -0,0 +1,13 @@
+package com.group6.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhLaptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhLaptopApplication.class, args);
+	}
+
+}
