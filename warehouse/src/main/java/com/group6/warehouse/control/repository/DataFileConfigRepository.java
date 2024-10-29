a5cee440b7ce61b53fa5c6f4b38a5c8f3ac1d997
BELCAO <caosybel@gmail.com>
2024-10-29 21:28:20 +0700

    delete entity

--- warehouse/src/main/java/com/group6/warehouse/control/repository/DataFileConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group6.warehouse.control.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.group6.warehouse.control.model.DataFileConfig;
-
-public interface DataFileConfigRepository extends JpaRepository<DataFileConfig, Long> {
-    // Bạn có thể thêm các phương thức tùy chỉnh nếu cần, ví dụ:
-    // List<DataFileConfig> findByName(String name);
-}
--- warehouse/src/main/java/com/group6/warehouse/datawarehouse/repository/ProductDimRepository.java
+++
@@ -1,8 +0,0 @@
-package com.group6.warehouse.datawarehouse.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.group6.warehouse.datawarehouse.model.ProductDim;
-
-public interface ProductDimRepository extends JpaRepository<ProductDim, Integer> {
-    // Các phương thức tùy chỉnh nếu cần
-}
