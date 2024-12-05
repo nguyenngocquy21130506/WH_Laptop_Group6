809a59f9d6b8393ef121a39ecc5becc865c05af6
21130453-HuynhBaoHuyenNgoc <21130453@st.hcmuaf.edu.vn>
2024-12-05 15:19:18 +0700

    update path

--- warehouse/src/main/java/com/group6/warehouse/loadStaging/ConfigReader.java
+++ warehouse/src/main/java/com/group6/warehouse/loadStaging/ConfigReader.java
@@ -8,7 +8,7 @@ public class ConfigReader {
     private static final Properties properties = new Properties();
 
     static {
-        try (FileInputStream fis = new FileInputStream("D:\\Nhom6_WH\\WH_Laptop_Group6\\warehouse\\src\\main\\resources\\application.properties")) {
+        try (FileInputStream fis = new FileInputStream("D:\\ServerWH\\WH_Laptop_Group6\\warehouse\\src\\main\\resources\\application.properties")) {
             properties.load(fis);
         } catch (IOException e) {
             System.err.println("Unable to load configuration file: " + e.getMessage());
